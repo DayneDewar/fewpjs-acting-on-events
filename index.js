c5f2a29897709b43f670e583261f5a7517800e2b
Dayne Dewar <daynedewar@yahoo.com>
2021-02-25 18:59:48 -0500

    commit tree 61d3ba4feeb00ac95c1dc2852f709ef2f78d4ea3

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+    let numberLeft = dodger.style.left.replace('px', '');
+    let left = parseInt(numberLeft, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    let numberLeft = dodger.style.left.replace('px', '');
+    let left = parseInt(numberLeft, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } 
+    else if (event.key === 'ArrowRight') {
+        moveDodgerRight();
+    } 
+})
